e4575819b190c03a2eb2df518a832962f6e63954
iLab.Utk <ilab.utk@gmail.com>
2022-10-21 14:35:30 -0400

    on ilab001 mac - config changes

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/shawn/Dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/shawn/Dev/gskinner/flutter-wonders-app"
+export "FLUTTER_ROOT=/Users/Shared/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ilab/Documents/GitHub/flutter-wonderous-app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.9.6"
-export "FLUTTER_BUILD_NUMBER=1.9.6"
+export "FLUTTER_BUILD_NAME=1.9.7"
+export "FLUTTER_BUILD_NUMBER=1.9.7"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
